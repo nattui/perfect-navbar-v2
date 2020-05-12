73df09f5491e534ce911890f6b7601592784e5bb
Nhat Nguyen <natt.lefevre@gmail.com>
2020-05-11 22:52:10 -0700

    🧪 Debugging Firebox mobile bug II

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,67 +1,72 @@
 console.log('navbar.js is loaded...');
 
-// TODO: clean up variables
-// Test Deployment
+// TODO: Add comments
 
-// Checking whether to add margin top to body
-const navbarFixed = document.getElementsByClassName('navbar--fixed')[0];
+
+// Variables
+// Navbar
+const body = document.getElementsByTagName('body')[0];
+const navbar = document.getElementsByClassName('navbar')[0];
+// const navbarFixed = document.getElementsByClassName('navbar--fixed')[0];
 const navbarBanner = document.getElementsByClassName('navbar__banner')[0];
 const navbarMain = document.getElementsByClassName('navbar__main')[0];
-const subnavbarWrapper = document.getElementsByClassName('subnavbar__wrapper')[0];
-let navbarHeight = 0;
-navbarBanner ? navbarHeight = navbarBanner.offsetHeight + navbarMain.offsetHeight : navbarMain.offsetHeight;
-const body = document.getElementsByTagName('body')[0];
-// console.log('navbar height:', navbarHeight);
-if (navbarFixed) body.style.marginTop = `${navbarHeight}px`;
-subnavbarWrapper.style.height = `calc(100% - ${navbarHeight}px)`;
-
-const something = (state) => {
-  for (let i = 0; i < subnavbar.children.length; i++) {
-    const n = subnavbar.children[i];
-    if (state) {
-      n.setAttribute('tabIndex', '-1');
-    } else {
-      n.removeAttribute('tabIndex');
-    }
-  }
-}
+const NAVBAR_SCROLL_AMOUNT = 16;
+const NAVBAR_BOX_SHADOW = '0 0 1px rgba(0, 0, 0, 0.04), 0 0 2px rgba(0, 0, 0, 0.06), 0 4px 8px rgba(0, 0, 0, 0.04)';
 
 // Subnavbar
-const navbar = document.getElementsByClassName('navbar')[0];
-const menu = document.getElementsByClassName('navbar__menu')[0];
-const menuClass = menu.getAttribute('class');
-const searchInput = document.getElementsByClassName('search__input')[0];
 const subnavbar = document.getElementsByClassName('subnavbar')[0];
-const html = document.getElementsByTagName('html')[0];
-// something(true);
+const subnavbarWrapper = document.getElementsByClassName('subnavbar__wrapper')[0];
+const menuButton = document.getElementsByClassName('navbar__menu')[0];
+const initialMenuClass = menuButton.getAttribute('class');
 
-let isMenuActive = false;
+// Searchbar
+const searchbar = document.getElementsByClassName('searchbar')[0];
+const searchbarInput = document.getElementsByClassName('searchbar__input')[0];
+const openSearchButton = document.getElementsByClassName('navbar__search')[0];
+const closeSearchButton = document.getElementsByClassName('searchbar__close')[0];
+
+
+// Set body top margins
+const resetBodyMargins = () => {
+  const navbarHeight = navbarBanner ?
+    navbarBanner.offsetHeight + navbarMain.offsetHeight :
+    navbarMain.offsetHeight;
+  body.style.marginTop = `${navbarHeight}px`;
+  subnavbarWrapper.style.height = `calc(100% - ${navbarHeight}px)`;
+  return navbarHeight;
+}
+
+resetBodyMargins();
+
+
+
+let isSubnavbarActive = false;
 const setSubnavbar = state => {
-  // window.scrollTo(0, 0);
   if (state) {
-    // Set menu icon to active
-    menu.setAttribute('class', `${menuClass} is-active`);
+    setSearchbar(false);
+    // Set menuButton icon to active
+    menuButton.setAttribute('class', `${initialMenuClass} is-active`);
     // Hide body scrollbar
     body.setAttribute('class', 'state--disableScroll');
 
     subnavbar.style.top = '100%';
     navbarMain.removeAttribute('style');
-    isMenuActive = true;
+    isSubnavbarActive = true;
   } else {
-    // Set menu icon to default state
-    menu.setAttribute('class', menuClass);
+    // Set menuButton icon to default state
+    menuButton.setAttribute('class', initialMenuClass);
     // Set body scrollbar to initial state
     body.removeAttribute('class');
 
-    subnavbar.style.top = `-100vh`;
-    window.pageYOffset > 8 ?
-      navbarMain.style.boxShadow = '0 0 1px rgba(0, 0, 0, 0.04), 0 0 2px rgba(0, 0, 0, 0.06), 0 4px 8px rgba(0, 0, 0, 0.04)' :
-      navbarMain.style.boxShadow = 'none';
-    isMenuActive = false;
+    subnavbar.removeAttribute('style');
+    window.pageYOffset > NAVBAR_SCROLL_AMOUNT ?
+      navbarMain.style.boxShadow = NAVBAR_BOX_SHADOW :
+      navbarMain.removeAttribute('style');
+    isSubnavbarActive = false;
   }
 }
 
-menu.addEventListener('click', () => setSubnavbar(!isMenuActive));
+menuButton.addEventListener('click', () => setSubnavbar(!isSubnavbarActive));
 
 
 // Resized event listener
@@ -72,42 +77,29 @@ window.addEventListener('resize', () => {
     windowWidth = window.innerWidth;
     setSubnavbar(false);
     setSearchbar(false);
-
-    // If navbar banner exist, adjust body margin top
-    navbarBanner ? navbarHeight = navbarBanner.offsetHeight + navbarMain.offsetHeight : navbarMain.offsetHeight;
-    body.style.marginTop = `${navbarHeight}px`;
-    subnavbarWrapper.style.height = `calc(100% - ${navbarHeight}px)`;
+    resetBodyMargins();
   }
 });
 
 
 // Add box shadow when scrolled down from initial position
-const SCROLL_AMOUNT = 8;
 window.addEventListener('scroll', () => {
-  window.pageYOffset > SCROLL_AMOUNT && !isMenuActive ?
-    navbarMain.style.boxShadow = '0 0 1px rgba(0, 0, 0, 0.04), 0 0 2px rgba(0, 0, 0, 0.06), 0 4px 8px rgba(0, 0, 0, 0.04)' :
-    navbarMain.style.boxShadow = 'none';
+  window.pageYOffset > NAVBAR_SCROLL_AMOUNT && !isSubnavbarActive ?
+    navbarMain.style.boxShadow = NAVBAR_BOX_SHADOW :
+    navbarMain.removeAttribute('style');
 });
 
 
-// Searchbar
-const searchbar = document.getElementsByClassName('searchbar')[0];
-const searchbarInput = document.getElementsByClassName('searchbar__input')[0];
-const openSearchButton = document.getElementsByClassName('navbar__search')[0];
-const closeSearchButton = document.getElementsByClassName('searchbar__close')[0];
-
 // Set the searchbar
 let isSeachbarActive = false;
-const setSearchbar = (state) => {
+const setSearchbar = state => {
   if (state) {
     setSubnavbar(false);
-    navbarBanner ?
-      searchbar.style.top = `${navbarBanner.offsetHeight}px` :
-      searchbar.style.top = '44px';
+    searchbar.style.top = navbarBanner ? `${navbarBanner.offsetHeight}px` : 0;
     searchbarInput.focus();
     isSeachbarActive = true;
   } else {
-    searchbar.style.top = '-64px';
+    searchbar.style.top = null;
     isSeachbarActive = false;
   }
 }
@@ -115,12 +107,43 @@ const setSearchbar = (state) => {
 openSearchButton.addEventListener('click', () => setSearchbar(true));
 closeSearchButton.addEventListener('click', () => setSearchbar(false));
 
+
+// Keyboard shortcuts
 window.addEventListener('keydown', event => {
-  let key = event.key;
-  if (key === 's') {
-    setSearchbar(true);
-  } else if (key === 'm') {
-    setSearchbar(false);
-    setSubnavbar(true);
+  switch (event.key) {
+    case '<':
+      setSearchbar(!isSeachbarActive);
+      setTimeout(() => searchbarInput.value = '', 1)
+      break;
+    case '>':
+      setSubnavbar(!isSubnavbarActive);
+      break;
+    case 'Escape':
+      setSearchbar(false);
+      setSubnavbar(false);
+      break;
   }
-}, { once: true });
+});
+
+
+// Close navbar banner
+if (navbarBanner) {
+  const closeBannerButton = document.getElementsByClassName('navbar__close')[0];
+  closeBannerButton.addEventListener('click', () => {
+    body.style.transition = 'margin-top 200ms';
+    body.style.marginTop = `${navbarMain.offsetHeight}px`;
+    navbar.style.transition = 'top 200ms';
+    navbar.style.top = `-${navbarBanner.offsetHeight}px`;
+    setTimeout(() => {
+      body.style.transition = null;
+      navbar.removeAttribute('style');
+      navbar.removeChild(navbarBanner);
+      subnavbarWrapper.style.height = `calc(100% - ${navbarMain.offsetHeight}px)`;
+      if (isSeachbarActive) {
+        searchbar.style.top = 0;
+        searchbar.style.transition = 'none';
+        setTimeout(() => searchbar.style.transition = null, 1);
+      }
+    }, 200);
+  }, { once: true });
+}
